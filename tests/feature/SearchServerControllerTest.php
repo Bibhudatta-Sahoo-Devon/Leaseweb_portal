3413f239f1de734e54a2f114f3c2f522b55b68b2
unknown <b.sahoo@devon.nl>
2022-03-21 19:15:35 +0530

    added feature test

--- tests/feature/SearchServerControllerTest.php
+++ tests/feature/SearchServerControllerTest.php
@@ -20,5 +20,40 @@ class SearchServerControllerTest extends WebTestCase
         $this->assertResponseStatusCodeSame(200);
     }
 
+    /**
+     * @test
+     */
+    public function search_with_all_filter()
+    {
+        $client =   $client = static::createClient();
+        $client->request('GET', '/api/search/servers?storage_size_from=1TB&ram_size[]=32gb&hdd_type=sata&location=AmsterdamAMS-01&storage_size_to=8TB&ram_size[]=4gb&ram_size[]=8gb');
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+
+    /**
+     * @test
+     */
+    public function search_with_ram_filter()
+    {
+        $client =   $client = static::createClient();
+        $client->request('GET', '/api/search/servers?ram_size[]=32gb&ram_size[]=4gb&ram_size[]=8gb');
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+
+    /**
+     * @test
+     */
+    public function search_with_harddisk_filter()
+    {
+        $client =   $client = static::createClient();
+        $client->request('GET', '/api/search/servers?storage_size_from=1TB&hdd_type=sata&storage_size_to=8TB');
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+
 
 }
